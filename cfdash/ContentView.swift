d9bf1ed819822ab88b949e796c8cd1481c83ab54
Lsong <song940@gmail.com>
2023-08-03 11:48:04 +0800

    Initial Commit

---
+++ cfdash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cfdash
+//
+//  Created by Lsong on 8/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cfdash/cfdashApp.swift
@@ -0,0 +1,17 @@
+//
+//  cfdashApp.swift
+//  cfdash
+//
+//  Created by Lsong on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct cfdashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
